c6f6c9942e0fee13217574fe30854c33edbb2b35
kkothari93 <krk3mar@gmail.com>
2018-01-30 02:35:32 -0600

    add vgg net for style transfer and clean-up code

--- style-transfer.py
+++ style-transfer.py
@@ -1,145 +1,133 @@
 """This file implements style-transfer by Gatys et al."""
 
 import tensorflow as tf
-import tensorflow.contrib.layers as layers
+import tensorflow.contrib.layers as l
 import numpy as np
 import scipy.misc as sc
 from PIL import Image
+from vgg import Vgg19, vggnet
 
-img_size = 256
+img_size = 224
 
-## deep net settings
-layer_sizes = [32,64,128,3]
-kernel_size = [32,16,16,4]
+
+# deep net settings
+layer_sizes = [32, 64, 128, 128]
+kernels = [5, 5, 5, 5]
 n = len(layer_sizes)
 
-## multi-scale settings
-n_channels_per_scale = 100
+
+NET = vggnet("./vgg19.npy", [3, 4])
+N_LAYERS = 2
+# ## multi-scale settings
+n_channels_per_scale = 20
 # use kernels in power of 2 and l kernels of each to get enough info
-kernels = [img_size//2**i for i in range(5, int(np.log(img_size)/np.log(2)))]
+kernels = [33,17,9,5]
 nkernels = len(kernels)
 
 
 def msnet(x):
     """Implement a shallow multi-scale convnet to capture style of an image"""
 
-    inp = tf.reshape(x, (1, img_size, img_size, 3))
-    style_features = tf.stack([tf.reshape(
-        layers.avg_pool2d(
-            layers.conv2d((inp), n_channels_per_scale,
-                          padding = "valid",
+    features = tf.stack([tf.reshape(
+        l.avg_pool2d(
+            l.conv2d(x, n_channels_per_scale,
+                          padding = "same",
                           kernel_size=kernels[i],
                           activation_fn=tf.nn.leaky_relu,
                           trainable=False,
+                          biases_initializer=tf.random_uniform_initializer(maxval=0.1),
                           reuse=tf.AUTO_REUSE,
                           scope='style%d' % i),
-            4),  # pool op
-        (-1, n_channels_per_scale))  # reshape op
+            2),  # pool op
+        (3, -1, n_channels_per_scale))  # reshape op
         for i in range(nkernels)]) # stack op
-    return style_features
+    
+    features=tf.transpose(features, (1,2,3,0))
 
+    return tf.reshape(features, (3, -1, n_channels_per_scale*nkernels))
 
 def net(x):
     """ Neural net for style and content representations
-    --  conv layers with channels as specified in layerswith leaky ReLU activations
+    --  conv l with channels as specified in l with leaky ReLU activations
     """
 
-    kernels = kernel_size
-
-    if type(kernel_size) == int:
-        kernels = [kernel_size, ]*n
-
-    elif type(kernel_size) == list:
-        if len(kernel_size) < n:
-            kernels += [kernel_size[-1], ]*(n-len(kernel_size))
-
-    else:
-        kernels = []
-
     outs = []
-    # out = tf.clip_by_value(tf.reshape(x,(1,img_size,img_size,3)),0,1)
-    out = tf.reshape(x, (1, img_size, img_size, 3))
+    out = x
     for i, nc in enumerate(layer_sizes):
-        out = layers.conv2d(out, nc, kernel_size=kernels[i],
-                            padding="valid",
+        out = l.conv2d(out, nc, kernel_size=kernels[i],
+                            padding="same",
                             activation_fn=tf.nn.leaky_relu,
                             trainable=False,
                             reuse=tf.AUTO_REUSE,
+                            biases_initializer=tf.random_uniform_initializer(
+                                maxval=1/sum(layer_sizes[i:i+2])),
                             scope='conv%d' % i)
-        out = layers.avg_pool2d(out, 4)
-        outs.append(tf.reshape(out, (-1, nc) ))
+        out = l.avg_pool2d(out, 2)
+        outs.append(tf.reshape(out, (3, -1, nc)))
 
     return outs
 
 
-def getgram(image_feature):
-    return tf.tensordot(image_feature, image_feature, axes=[[1], [1]])
+def getgram(image_feature, axis=0):
+    return tf.tensordot(image_feature, image_feature, axes=[[axis], [axis]])
 
 
 def merge(image, content, style, loss_at_level, nn):
 
-    n = len(kernels)
-
     with tf.name_scope('apply_nn'):
-        outc = nn(content)
-        outx = nn(image)
-        outs = nn(style)
+        outputs = nn(tf.stack((content, style, image)))
+
     
     with tf.name_scope('merge'):
+        content_loss = style_loss = 0
 
-        if loss_at_level == "all":
-            if type(outs) == list:
-                content_loss = style_loss = 0
+        if nn.__name__!='msnet':
 
-                # use for loop
-                for i in range(len(outc)):
-                    content_loss += tf.reduce_sum((outc[i]-outx[i])**2)*0.5
-                    style_loss += tf.reduce_sum((getgram(outs[i]) - getgram(outx[i]))**2)*0.25/tf.reduce_sum(getgram(outs[i])**2)
+            content_loss = tf.nn.l2_loss(outputs[0][0] - outputs[0][2])
+            # use for loop
+            for i in range(N_LAYERS):
+                print(outputs[i].get_shape().as_list())
+                outc = outputs[i][0]
+                outs = outputs[i][1]
+                outx = outputs[i][2]
+                N1, N2 = outc.get_shape().as_list()
 
-            else:
-                # if not list, use tensor ops
-
-                # use loss from all levels/ all scales
-                content_loss = tf.reduce_sum((outc - outx)**2)*0.5
-                style_loss =  tf.reduce_sum((getgram(outs) - getgram(outx))**2)*0.25/tf.reduce_sum(getgram(outs)**2)
+                # content_loss += tf.nn.l2_loss(outc-outx)
+                Gs = getgram(outs)
+                Gx = getgram(outx)
+                style_loss += tf.nn.l2_loss(Gs-Gx) / (2*N1*N1*N2*N2)
 
         else:
-            content_loss = tf.reduce_sum(
-                outc[loss_at_level] - outx[loss_at_level])**2*0.5
-            g = tf.cast(tf.shape(outs[loss_at_level]), 'float32')
-            style_loss = tf.reduce_sum(
-                (getgram(outs[loss_at_level]) - getgram(outx[loss_at_level]))**2)*0.25/(g[1]**2)/(g[0]**2)
+            outc, outs, outx = tf.split(axis=0, num_or_size_splits=3, value=outputs)
+            _, N1, N2 = outc.get_shape().as_list() # gets size 1 axis 0
+            content_loss += tf.nn.l2_loss(outc-outx)
+            Gs = getgram(outs)
+            Gx = getgram(outx)
+            style_loss += tf.nn.l2_loss(Gs-Gx) / (2*N1*N1*N2*N2)
 
-    return content_loss, style_loss
+    return content_loss, style_loss, outputs
 
 
-def train(im, content, style, nn, a=0.0, loss_at_level="all"):
+def train(loss):
+    return tf.train.AdamOptimizer(learning_rate=1e-1).minimize(loss)
 
-    content_loss, style_loss = merge(im, content, style, loss_at_level, nn)
-    
-    with tf.name_scope('optimize'):
-        loss = a*content_loss + (1-a)*style_loss*1e3
-        # loss = tf.maximum(content_loss, style_loss*1e3, name="loss")
-        train_step = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(loss)
-
-    return train_step,  loss, content_loss, style_loss
 
 def visualize(out, desc='content'):
     if type(out) == list:
         nk, nf, nc = len(out), len(out[0]), len(out[0][0])
     else:
-        nk, nf, nc = out.shape # nkernels, nfeatures, channels_per_kernel
+        nk, nf, nc = out.shape  # nkernels, nfeatures, channels_per_kernel
 
     for k in range(nk):
         outk = out[k]
         # figure out how big a canvas is needed for the feature
-        s = len(outk[:,0])
+        s = len(outk[:, 0])
         s = int(np.sqrt(s))
         ncols = 5
         nrows = int(np.ceil(nc/float(ncols))) + 1
 
-        total_height = s*ncols + (ncols-1)*5 # 5 px gap between two outputs
-        total_width = s*nrows + (nrows-1)*5 # 5 px gap between two outputs
+        total_height = s*ncols + (ncols-1)*5  # 5 px gap between two outputs
+        total_width = s*nrows + (nrows-1)*5  # 5 px gap between two outputs
 
         new_im = Image.new('P', (total_height, total_width))
 
@@ -154,34 +142,7 @@ def visualize(out, desc='content'):
             im = Image.fromarray(np.uint8(g))
             new_im.paste(im, box=offset)
 
-        new_im.save('activation_%s_kernel%d.png' % (desc, kernels[k]) )
-
-
-
-# def visualize(out, desc='content'):
-#     last_layer = out[-1]
-#     s, nc = last_layer.shape
-#     ncols = 8
-#     nrows = int(np.ceil(nc/float(ncols))) + 1
-
-#     s = int(np.sqrt(s))
-#     # height and width are opposites in Image lib
-#     total_height = s*ncols + (ncols-1)*5
-#     total_width = s*nrows + (nrows-1)*5
-
-#     new_im = Image.new('P', (total_width, total_height))
-#     for i in range(nc):
-#         c = i % ncols
-#         r = i//ncols
-#         x_offset = c*s+5
-#         y_offset = r*s+5
-#         g = last_layer[:, i].reshape(s, s)
-#         g = (g-g.min())/(g.max()-g.min())*255
-#         im = Image.fromarray(np.uint8(g))
-#         new_im.paste(im, box=(x_offset, y_offset+s))
-
-#     new_im.save('corrupted_%s.png' % desc)
-#     return
+        new_im.save('activation_%s_kernel%d.png' % (desc, k))
 
 
 def activations(sess, ops, feed_dict, labels):
@@ -192,39 +153,40 @@ def activations(sess, ops, feed_dict, labels):
         visualize(result, desc=labels[i])
 
 
-def style_transfer(c, s, nn=msnet, mix=0.5):
+def style_transfer(c, s, nn=vggnet, mix=(10, 100, 1)):
     """transfer style of s onto c"""
     h, w, nc = c.shape
 
     tf.reset_default_graph()
 
-    content = tf.placeholder('float32')
-    style = tf.placeholder('float32')
-    x = tf.Variable(tf.random_uniform((1, h, w, nc)), name='input')
+    content = tf.placeholder('float32', shape=(h, w, nc))
+    style = tf.placeholder('float32', shape=(h, w, nc))
+    x = tf.Variable(tf.random_uniform((h, w, nc)), name='input')
 
-    op_train, op_loss, op_content, op_style = train(
-        x, content, style, nn, a=mix, loss_at_level="all")
-    op_c, op_s, op_x = nn(content), nn(style), nn(x)
+    lc, ls, outs = merge(x, content, style, -1, nn)
+
+    # content + perceptual + tv loss
+    loss = mix[0]*lc + mix[1]*ls + mix[2]*tf.image.total_variation(x)
+    train_step = train(loss)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
         with tf.name_scope('optimize'):
-            # visualize output of our content and style filters
-            activations(sess, [op_c, op_s], {content: c, style: s}, ['content', 'style'])
 
             # run optimization
             for ii in range(50001):
-                _, loss, closs, sloss = sess.run(
-                    [op_train, op_loss, op_content, op_style],
+                closs, sloss, tloss, _, _ = sess.run(
+                    [lc, ls, loss, outs, train_step],
                     feed_dict={content: c, style: s})
-                if ii % 1000 == 0 or loss < 1e-6:
-                    activations(sess, [op_x], {}, ['variable'])
+                if ii % 1000 == 0 or tloss < 1e-6:
+
                     print("loss @ iteration %d = %f, in content = %f, in style = %f" %
-                          (ii+1, loss, closs, sloss))
+                          (ii+1, tloss, closs, sloss))
+
                     out = sess.run(x)
-                    sc.imsave('output.jpg', out.reshape(img_size, img_size, 3))
-                    if loss < 1e-6:
+                    sc.imsave('output.jpg', out.reshape(h, w, nc))
+                    if tloss < 1e-6:
                         break
 
     return out
@@ -235,19 +197,18 @@ def main():
                           (img_size, img_size))/255.0
     sc.imsave('content.jpg', content)
 
-    # corruption
-    p = 0.1
-    npixels = img_size**2
-    pixel_index = np.random.randint(0, npixels*3, int(p*npixels*3))
-    channel = pixel_index//npixels
-    row = (pixel_index - channel*npixels)//img_size
-    col = (pixel_index - channel*npixels)%img_size
-    print(content[row, col, channel].shape)
-    content[row, col, channel] = 0.0
+    # # corruption
+    # p = 0.1
+    # npixels = img_size**2
+    # pixel_index = np.random.randint(0, npixels*3, int(p*npixels*3))
+    # channel = pixel_index//npixels
+    # row = (pixel_index - channel*npixels)//img_size
+    # col = (pixel_index - channel*npixels)%img_size
+    # print(content[row, col, channel].shape)
+    # content[row, col, channel] = 0.0
 
-    # save corrupted image
-    sc.imsave('corrupted_content.jpg', content)
-
+    # # save corrupted image
+    # sc.imsave('corrupted_content.jpg', content)
 
     style = sc.imresize(sc.imread('starry_night.jpg'),
                         (img_size, img_size))/255.0
@@ -255,7 +216,7 @@ def main():
     #                     (img_size, img_size))/255.0
     sc.imsave('style.jpg', style)
 
-    out = style_transfer(content, style, nn=net, mix=0.5)
+    out = style_transfer(content, style, nn=NET, mix=(1, 100, 0.001))
     return
 
 if __name__ == '__main__':
---
+++ vgg.py
@@ -0,0 +1,151 @@
+import os
+import tensorflow as tf
+
+import numpy as np
+import time
+import inspect
+
+VGG_MEAN = [103.939, 116.779, 123.68]
+
+
+class Vgg19:
+
+    def __init__(self, vgg19_npy_path=None, layers=[]):
+        if vgg19_npy_path is None:
+            path = inspect.getfile(Vgg19)
+            path = os.path.abspath(os.path.join(path, os.pardir))
+            path = os.path.join(path, "vgg19.npy")
+            vgg19_npy_path = path
+            print(vgg19_npy_path)
+
+       	self.layers = layers
+        self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+
+        print("npy file loaded")
+
+    def build(self, rgb):
+        """
+        load variable from npy to build the VGG
+        :param rgb: rgb image [batch, height, width, 3] values scaled [0, 1]
+        """
+        start_time = time.time()
+        print("build model started")
+        rgb_scaled = rgb * 255.0
+
+        # Convert RGB to BGR
+        red, green, blue = tf.split(
+            axis=3, num_or_size_splits=3, value=rgb_scaled)
+        # assert red.get_shape().as_list()[1:] == [224, 224, 1]
+        # assert green.get_shape().as_list()[1:] == [224, 224, 1]
+        # assert blue.get_shape().as_list()[1:] == [224, 224, 1]
+        bgr = tf.concat(axis=3, values=[
+            blue - VGG_MEAN[0],
+            green - VGG_MEAN[1],
+            red - VGG_MEAN[2],
+        ])
+        # assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
+
+        self.conv1_1 = self.conv_layer(bgr, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
+        self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+
+        self.conv2_1 = self.conv_layer(self.pool1, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, "conv2_2")
+        self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+
+        self.conv3_1 = self.conv_layer(self.pool2, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, "conv3_4")
+        self.pool3 = self.max_pool(self.conv3_4, 'pool3')
+
+        self.conv4_1 = self.conv_layer(self.pool3, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, "conv4_3")
+        self.conv4_4 = self.conv_layer(self.conv4_3, "conv4_4")
+        self.pool4 = self.max_pool(self.conv4_4, 'pool4')
+
+        self.conv5_1 = self.conv_layer(self.pool4, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, "conv5_3")
+        self.conv5_4 = self.conv_layer(self.conv5_3, "conv5_4")
+        self.pool5 = self.max_pool(self.conv5_4, 'pool5')
+
+        self.data_dict = None
+
+        self.ops = []
+
+    	for val in self.layers:
+    		if val == 1:
+    			a, b, c, d = self.conv1_2.get_shape().as_list()
+    			self.ops.append(tf.reshape(self.conv1_2, (a, b*c, d)))
+    		elif val == 2:
+    			a, b, c, d = self.conv2_2.get_shape().as_list()
+    			self.ops.append(tf.reshape(self.conv2_2, (a, b*c, d)))
+    		elif val == 3:
+    			a, b, c, d = self.conv3_4.get_shape().as_list()
+    			self.ops.append(tf.reshape(self.conv3_4, (a, b*c, d)))
+    		elif val == 4:
+    			a, b, c, d = self.conv4_4.get_shape().as_list()
+    			self.ops.append(tf.reshape(self.conv4_4, (a, b*c, d)))
+    		elif val == 5:
+    			a, b, c, d = self.conv5_4.get_shape().as_list()
+    			self.ops.append(tf.reshape(self.conv5_4, (a, b*c, d)))
+
+        print(("build model finished: %ds" % (time.time() - start_time)))
+
+        return self.ops
+
+    def avg_pool(self, bottom, name):
+        return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+    def max_pool(self, bottom, name):
+        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
+
+    def conv_layer(self, bottom, name):
+        with tf.variable_scope(name):
+            filt = self.get_conv_filter(name)
+
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+
+            conv_biases = self.get_bias(name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
+            return relu
+
+    def get_conv_filter(self, name):
+        return tf.constant(self.data_dict[name][0], name="filter")
+
+    def get_bias(self, name):
+        return tf.constant(self.data_dict[name][1], name="biases")
+
+    def get_fc_weight(self, name):
+        return tf.constant(self.data_dict[name][0], name="weights")
+
+
+def vggnet(path, layers):
+	vgg19 = Vgg19(vgg19_npy_path=path, layers=layers)
+	return vgg19.build
+
+
+class randVgg19(Vgg19):
+
+	def __init__(self, path="./vgg19.npy", layers=[]):
+		super.__init__(path, layers)
+		self.sizes = [64, 128, 256, 512, 512]
+		self.layers = layers
+
+	def conv_layer(self, bottom, name):
+        with tf.variable_scope(name):
+        	blockid = int(name[-3])
+            out = tf.contrib.layers.conv2d(bottom, self.sizes[block],kernel_size=kernels[i],
+                            padding="same",
+                            activation_fn=tf.nn.relu,
+                            trainable=False,
+                            reuse=tf.AUTO_REUSE,
+                            biases_initializer=tf.random_uniform_initializer(
+                                maxval=0.1),
+                            scope='conv%d' % i)
+
+            return out
